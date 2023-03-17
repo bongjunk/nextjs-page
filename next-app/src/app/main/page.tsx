3e6403b2b26e1856c4631449fa5752830afe4fe6
bongjunk <mydlwm_s@naver.com>
2023-03-17 18:04:50 +0900

    nextjs 13 setting

---
+++ next-app/src/app/main/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const DivS = styled.div`
+  background-color: #ddd;
+  height: 100%;
+`;
+
+const main = () => {
+  return (
+    <>
+      <DivS>
+        <p>nextjs main</p>
+        <p>nextjs main</p>
+        <p>nextjs main</p>
+        <p>nextjs main</p>
+        <p>nextjs main</p>
+      </DivS>
+    </>
+  );
+};
+
+export default main;
---
+++ next-app/src/app/sub/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const SubBox = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background-color: #000;
+`;
+
+const sub = () => {
+  return (
+    <>
+      <div>
+        <p>sub page</p>
+        <SubBox />
+      </div>
+    </>
+  );
+};
+
+export default sub;
